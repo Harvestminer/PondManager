ec038b0463678fe0abb87e4ae6b0a3af35e5ff8e
Harvestminer <harvestminer@gmail.com>
2024-03-29 23:20:49 -0500

    Adds project files

---
+++ src/main/java/me/harvestminer/pondmanager/PondManager.java
@@ -0,0 +1,17 @@
+package me.harvestminer.pondmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PondManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
